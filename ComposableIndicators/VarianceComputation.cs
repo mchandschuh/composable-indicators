c16a88327be535bc41db329b238f5b7c58a64173
Michael Handschuh <mhandschuh@gmail.com>
2017-08-06 22:49:06 -0400

    Add VarianceComputation

---
+++ ComposableIndicators/VarianceComputation.cs
@@ -0,0 +1,21 @@
+﻿namespace ComposableIndicators
+{
+    public class VarianceComputation : IWindowComputation
+    {
+        private readonly MeanComputation meanComputation = new MeanComputation();
+        private readonly SumOfSquaresComputation sumOfSquaresComputation = new SumOfSquaresComputation();
+
+        public double? Compute(RollingWindow<double> window)
+        {
+            var mean = meanComputation.Compute(window);
+            var sumOfSquares = sumOfSquaresComputation.Compute(window);
+
+            if (window.Samples < 2)
+            {
+                return null;
+            }
+
+            return sumOfSquares/window.Count - mean * mean;
+        }
+    }
+}
