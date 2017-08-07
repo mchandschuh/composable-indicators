cda28ae51179c0173f9002974a9070578a24f108
Michael Handschuh <mhandschuh@gmail.com>
2017-08-06 21:58:23 -0400

    Add SumComputation
    Provides an on-line, running sum computation. Keeping a running sum is fairly
    common for indicators to operate in an on-line fashion.

---
+++ ComposableIndicators.Tests/SumComputationTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ComposableIndicators.Tests
+{
+    [TestFixture]
+    public class SumComputationTests
+    {
+        [Test]
+        public void WhenComputing_Then_ResultIsTheSumOfAllDataInTheWindow()
+        {
+            const int period = 5;
+            var sut = new SumComputation();
+
+            var window = new RollingWindow<double>(period);
+            for (int i = 0; i < 2*period; i++)
+            {
+                window.Add(i);
+
+                var result = sut.Compute(window);
+                Assert.AreEqual(window.Sum(), result);
+            }
+        }
+    }
+}
---
+++ ComposableIndicators/SumComputation.cs
@@ -0,0 +1,19 @@
+﻿namespace ComposableIndicators
+{
+    public class SumComputation : IWindowComputation
+    {
+        private double sum;
+
+        public double? Compute(RollingWindow<double> window)
+        {
+            double removed = 0;
+            if (window.HasOverflowed)
+            {
+                removed = window.MostRecentlyRemoved;
+            }
+
+            sum = sum + window[0] - removed;
+            return sum;
+        }
+    }
+}
