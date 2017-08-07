797e759c86c180cf5d8871469a14bb226128f327
Michael Handschuh <mhandschuh@gmail.com>
2017-08-06 22:20:15 -0400

    Add SumOfSquaresComputation
    Summing the squares is a building block for variance, which in
    turn is a building block for the standard deviation

---
+++ ComposableIndicators.Tests/SumOfSquaresComputationTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ComposableIndicators.Tests
+{
+    [TestFixture]
+    public class SumOfSquaresComputationTests
+    {
+        [Test]
+        public void WhenComputing_Then_ResultIsTheSumOfAllDataInTheWindow()
+        {
+            const int period = 5;
+            var sut = new SumOfSquaresComputation();
+
+            var window = new RollingWindow<double>(period);
+            for (int i = 0; i < 2 * period; i++)
+            {
+                window.Add(i);
+
+                var result = sut.Compute(window);
+                Assert.AreEqual(window.Select(x => x * x).Sum(), result);
+            }
+        }
+    }
+}
---
+++ ComposableIndicators/SumOfSquaresComputation.cs
@@ -0,0 +1,19 @@
+﻿namespace ComposableIndicators
+{
+    public class SumOfSquaresComputation : IWindowComputation
+    {
+        private double sumOfSquares;
+
+        public double? Compute(RollingWindow<double> window)
+        {
+            double removed = 0;
+            if (window.HasOverflowed)
+            {
+                removed = window.MostRecentlyRemoved;
+            }
+
+            sumOfSquares = sumOfSquares + window[0]*window[0] - removed*removed;
+            return sumOfSquares;
+        }
+    }
+}
