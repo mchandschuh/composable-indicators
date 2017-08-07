3ccb8dd18ccc623764ebb1db31948dab3631e577
Michael Handschuh <mhandschuh@gmail.com>
2017-08-06 22:53:41 -0400

    Add StandardDeviationComputation
    Standard deviation of population

---
+++ ComposableIndicators.Tests/StandardDeviationComputationTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Accord.Statistics;
+using NUnit.Framework;
+
+namespace ComposableIndicators.Tests
+{
+    [TestFixture]
+    public class StandardDeviationComputationTests
+    {
+        [Test]
+        public void WhenComputing_Then_ResultIsTheStandardDeviationUsingPopulationVarianceOfAllDataInTheWindow()
+        {
+            const int period = 5;
+            var sut = new StandardDeviationComputation();
+
+            var window = new RollingWindow<double>(period);
+            window.Add(0);
+            Assert.AreEqual(null, sut.Compute(window));
+
+            for (int i = 1; i < 2 * period; i++)
+            {
+                window.Add(i);
+
+                var actual = sut.Compute(window);
+                var expected = window.ToArray().StandardDeviation(false);
+                Assert.AreEqual(expected, actual, 1e-10);
+            }
+        }
+    }
+}
---
+++ ComposableIndicators/StandardDeviationComputation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ComposableIndicators
+{
+    public class StandardDeviationComputation : IWindowComputation
+    {
+        private readonly VarianceComputation varianceComputation = new VarianceComputation();
+
+        public double? Compute(RollingWindow<double> window)
+        {
+            var variance = varianceComputation.Compute(window);
+            if (variance == null)
+            {
+                return null;
+            }
+
+            return Math.Sqrt(variance.Value);
+        }
+    }
+}
